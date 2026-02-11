94f86c3828f3b22b5f063c1a44e4d1d4eec53e1b
Erick Aróstegui Cunza <erick.arostegui.cunza@gmail.com>
2026-02-11 19:16:41 +0100

    Add page not found

--- src/app/ui/main/routes/app.routes.ts
+++ src/app/ui/main/routes/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { Layout } from '../shell/layout/layout';
 import { Home } from '../pages/home/home';
+import { PageNotFound } from '@shared/pages/page-not-found';
 
 export const routes: Routes = [
   { path: '', component: Home },
-  { path: 'library',component: Layout }
+  { path: 'library',component: Layout },
+  { path: '**', component: PageNotFound }
 ];
